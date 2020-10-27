7911ae058449f08d99cbcdf212365e1712b92990
mihailgaberov <mihail.gaberov@gmail.com>
2020-10-27 08:36:54 +0100

    :art: Use arrow function callback.

--- calc.js
+++ calc.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 
 calculate = () => {
-    fs.readFile('data.txt', 'utf8', function(err, data) {
+    fs.readFile('data.txt', 'utf8', (err, data) => {
         if (err) {
-            return console.log(err);
+            throw new Error(err)
         }
+
         const arr = data.split('\r\n');
         const result = arr
             .filter(e => e)
